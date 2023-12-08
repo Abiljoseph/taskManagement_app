4196900dc4b66905efe2ffca95f347518c62c0d6
Abil joseph <abiljoseph98.p@gmail.com>
2023-12-08 14:38:25 +0530

    url updation

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://task-backend-uukk.onrender.com",
         secure: false,
+        rejectUnauthorized: false,
       },
     },
   },
